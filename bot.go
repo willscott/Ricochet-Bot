488d7a71418975787dfc529ec19d102d6471fcda
Will Scott <willscott@gmail.com>
2017-03-28 21:10:19 -0700

    bot now acts like a chat room.

--- bot.go
+++ bot.go
@@ -10,22 +10,61 @@ import (
 // TrebuchetBot represents the bot logic.
 type TrebuchetBot struct {
 	goricochet.StandardRicochetService
+	knownContacts  []string
+	activeContacts []*TrebuchetConnection
 }
 
 // OnNewConnection starts a thread for each new connection.
 func (tb *TrebuchetBot) OnNewConnection(oc *goricochet.OpenConnection) {
+	if tb.activeContacts == nil {
+		tb.activeContacts = make([]*TrebuchetConnection, 0, 1)
+		tb.knownContacts = make([]string, 0)
+	}
+	//if !stringInSlice(tc.Conn.OtherHostname, tc.TrebuchetBot.knownContacts) {
+	//	tc.TrebuchetBot.knownContacts = append(tc.TrebuchetBot.knownContacts, tc.Conn.OtherHostname)
+	//}
 	tb.StandardRicochetService.OnNewConnection(oc)
-	go oc.Process(&TrebuchetConnection{})
+	tc := &TrebuchetConnection{goricochet.StandardRicochetConnection{}, tb, -1, oc.OtherHostname}
+
+	for i := 0; i < len(tb.activeContacts); i++ {
+		tb.activeContacts[i].send(tc.Conn.OtherHostname + " joined the room.")
+	}
+	tb.activeContacts = append(tb.activeContacts, tc)
+	go oc.Process(tc)
 }
 
 // TrebuchetConnection represents a connection made by TrebucetBot
 type TrebuchetConnection struct {
 	goricochet.StandardRicochetConnection
+	*TrebuchetBot
+	outboundChatChannel int32
+	nick                string
+}
+
+func (tc *TrebuchetConnection) send(msg string) {
+	if tc.outboundChatChannel == -1 {
+		cid := int32(6)
+		if tc.Conn.Client {
+			cid++
+		}
+		tc.Conn.OpenChatChannel(cid)
+		tc.outboundChatChannel = cid
+	}
+	tc.Conn.SendMessage(tc.outboundChatChannel, msg)
 }
 
 // IsKnownContact Always Accepts Contact Requests
 func (tc *TrebuchetConnection) IsKnownContact(hostname string) bool {
 	return true
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
 }
 
 // OnContactRequest fires on contact requests
@@ -40,8 +79,22 @@ func (tc *TrebuchetConnection) OnContactRequest(channelID int32, nick string, me
 func (tc *TrebuchetConnection) OnChatMessage(channelID int32, messageID int32, message string) {
 	log.Printf("Received Message from %s: %s", tc.Conn.OtherHostname, message)
 	tc.Conn.AckChatMessage(channelID, messageID)
-	if tc.Conn.GetChannelType(6) == "none" {
-		tc.Conn.OpenChatChannel(6)
+	for i := 0; i < len(tc.activeContacts); i++ {
+		if tc.activeContacts[i] != tc {
+			tc.activeContacts[i].send(tc.nick + ": " + message)
+		}
 	}
-	tc.Conn.SendMessage(6, message)
+}
+
+// OnDisconnect fires when the remote connection closes.
+func (tc *TrebuchetConnection) OnDisconnect() {
+	for i := 0; i < len(tc.TrebuchetBot.activeContacts); i++ {
+		if tc.TrebuchetBot.activeContacts[i] == tc {
+			copy(tc.TrebuchetBot.activeContacts[i:], tc.TrebuchetBot.activeContacts[i+1:])
+			tc.TrebuchetBot.activeContacts[len(tc.TrebuchetBot.activeContacts)-1] = nil
+			tc.TrebuchetBot.activeContacts = tc.TrebuchetBot.activeContacts[:len(tc.TrebuchetBot.activeContacts)-1]
+			break
+		}
+	}
+	tc.StandardRicochetConnection.OnDisconnect()
 }
